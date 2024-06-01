19d1fcb9aa572f546b514afc556f54186380efa9
egorm <egormorozov123450@gmail.com>
2024-06-01 12:35:44 +0300

    create project

---
+++ src/main/java/com/example/Telegam_Bot/TelegamBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.Telegam_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegamBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegamBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Telegam_Bot/TelegamBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Telegam_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegamBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
